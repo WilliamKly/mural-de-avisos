5a0e87f38b944860dc9387f6c0cb20a3a00a3547
William Klywerston <williamklywerston10@gmail.com>
2022-06-01 12:53:08 -0300

    primeiro commit

---
+++ model/posts.js
@@ -0,0 +1,20 @@
+module.exports = {
+    posts: [
+        {id: "aleatório",
+        title: "Teste do Mural",
+        description: "Descrição teste"},
+    ],
+
+    getAll(){
+        return this.posts;
+    },
+
+    newPost(title, description,){
+        this.posts.push({id: generateID(), title, description});
+    }
+}
+
+
+function generateID(){
+    return Math.random().toString(36).substring(2, 9);
+}
